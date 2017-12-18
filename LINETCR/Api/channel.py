bc642ec96e8c9f42608a2735f06793d5aef6d0e3
adekrisna <32455883+adekrisna@users.noreply.github.com>
2017-12-18 11:38:18 +0700

    Add files via upload

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -20,6 +20,8 @@ class Channel:
 
     UA = "Line/7.14.0"
     LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
+    #UA = "Line/6.0.0 iPad4,1 9.0.2"
+    #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
     authToken = None
     mid = None
